7e24fd72040dcdd29f442b05448c50a150215bac
Doan Van Thuong <doanvanthuong196@gmail.com>
2016-12-27 21:56:17 +0700

    sua nut refresh them NV

--- QLNH/QL Nhà Hàng/QLNhanVien.cs
+++ QLNH/QL Nhà Hàng/QLNhanVien.cs
@@ -202,7 +202,11 @@ namespace QL_Nhà_Hàng
             radioMa.Checked = false;
             radioTen.Checked = false;
             txtSearch.Text = " ";
-
+            txtMaNV.Text = " ";
+            txtHo.Text = " ";
+            txtTen.Text = " ";
+            txtDiaChi.Text = " ";
+            txtSDT.Text = " ";
         }
 
      
