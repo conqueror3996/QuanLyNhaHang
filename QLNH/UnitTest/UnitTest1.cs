9a0d282a47b326d9f3c023a2999b5298bedaeaef
Nguyen Manh Khang <1451010085khang@ou.edu.vn>
2017-11-20 10:30:41 +0700

    up

---
+++ QLNH/UnitTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QL_Nhà_Hàng;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {       
+        class_khachhang kh = new class_khachhang();
+        private Login dn;
+        [TestInitialize]
+        public void setup()
+        {
+            dn = new Login();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(dn.ktdangnhap("admin","123456"), true);
+        }
+        public void TestMethod2()
+        {
+            Assert.AreEqual(dn.ktdangnhap("gtdrgte", "1te"), false);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(kh.kiemtra("", "", "", "", ""), true);
+        }
+    }
+}
