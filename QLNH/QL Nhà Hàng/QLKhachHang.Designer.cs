743af325862b3d68f7193c9fc4240e0b55b34c57
Nguyen Manh Khang <1451010085khang@ou.edu.vn>
2017-11-21 08:15:34 +0700

    last check

--- QLNH/QL Nhà Hàng/QLKhachHang.Designer.cs
+++ QLNH/QL Nhà Hàng/QLKhachHang.Designer.cs
@@ -297,7 +297,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(471, 274);
             this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
             // groupBox2
             // 
